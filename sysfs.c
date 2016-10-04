25ddec8b4370fcdd0a1088c43a9c3524f527105c
Andrewli <nyps70414@gmail.com>
2016-10-04 09:44:07 +0800

    successly makfile and install module

--- sysfs.c
+++ sysfs.c
@@ -5,24 +5,23 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-
-
+static int q1;
 static struct kobject *hw_kobject;
-static struct ssize_t q1_show(struct kobject *kobject,
+static ssize_t q1_show(struct kobject *kobject,
 			struct kobj_attribute *attr, char *buf)
 {
-
+	return sprintf(buf,"q1 show %s:%d\n",attr->attr.name,q1);
 }
-static struct ssize_t q1_store(struct kobject *kobject,
+static ssize_t q1_store(struct kobject *kobject,
 			struct kobj_attribute *attr, char *buf, size_t count)
 {
-
+	sprintf(buf,"%du\n",q1);
+	return count;
 }
+static struct kobj_attribute hw_attr = __ATTR(q1,0660,q1_show,q1_store);
 static int __init hw_init(void)
 {
 	hw_kobject = kobject_create_and_add("hw1",kernel_kobj);
-	struct kobj_attribute hw_attr = 
-		__ATTR(q1,0660,q1_show,q1_store);
 	sysfs_create_file(hw_kobject, &hw_attr.attr);
 }
 static void __exit hw_exit(void)
