e1d71e21789b1b630793d88b272f4bdeb0b37861
andrewli <nyps70414@gmail.com>
2016-10-14 15:25:19 +0800

    success first lab

--- sysfs.c
+++ sysfs.c
@@ -15,34 +15,34 @@ char* concat(char*,char*);
 static ssize_t s_show(struct kobject*,
 			struct kobj_attribute*,char*);
 static ssize_t s_store(struct kobject*,
-			struct kobj_attribute*, char*,size_t);
+			struct kobj_attribute*, const char*,size_t);
 
 static ssize_t s_show(struct kobject *kobject,
 			struct kobj_attribute *attr, char *buf)
 {
 	int i;
-	int len;
+	int len=0;
 	char str[100];
 	char arr[100];
 	concat(str,test);
-	sprintf(buf,"%s\n",str);
+
 	len = (int)strlen(str)-1;
 	for(i=0;i<len;i++)
 	{
-		if((i+3) < len)
-			arr[i+3] = *(str+i);
+		if((i+s) < len)
+			arr[i+s] = *(str+i);
 		else
-			arr[(i+3)%len] = *(str+i);
+			arr[(i+s)%len] = *(str+i);
 
 	}
-	arr[len+1]='\0';
-	return sprintf(buf,"str: %s\n",len,arr);
+	arr[len]='\0';
+	return sprintf(buf,"String: %s\n",arr);
 }
 static ssize_t s_store(struct kobject *kobject,
-			struct kobj_attribute *attr, char *buf, size_t count)
+			struct kobj_attribute *attr,const char *buf, size_t count)
 {
 	sscanf(buf,"%du",&s);
-	memcpy(test,buf,strlen(buf)+1);
+	memcpy(test,buf,strlen(buf));
 	return count;
 }
 static struct kobj_attribute hw_attr =
@@ -51,14 +51,14 @@ static int __init hw_init(void)
 {
 	int retval;
 	hw_kobject = kobject_create_and_add("hw1",kernel_kobj);
-	if(!hw_kobject)
-		return -ENOMEM;
+	
 	retval = sysfs_create_file(hw_kobject, &hw_attr.attr);
 	return retval;
 }
 static void __exit hw_exit(void)
 {
-	printk(KERN_ALERT "CLEAN UP MODULE\n");
+		printk(KERN_ALERT "CLEAN UP MODULE");
+		kobject_put(hw_kobject);
 }
 void swap_char(char *a,char *b)
 {
@@ -94,6 +94,7 @@ char* concat(char*a, char*b)
 	*(a+j+1) = '\0';
 	return a;
 }
+
 module_init(hw_init);
 module_exit(hw_exit);
 
