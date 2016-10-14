1f32aefd0beae48c33d2cfc963b47cd7d5fce575
Andrew Li <nyps70414@gmail.com>
2016-10-14 15:43:53 +0800

    complete 80%

--- sysfs.c
+++ sysfs.c
@@ -12,10 +12,23 @@ static struct kobject *hw_kobject;
 void swap_char(char *a,char *b);
 int atoi(char *str);
 char* concat(char*,char*);
+
 static ssize_t s_show(struct kobject*,
 			struct kobj_attribute*,char*);
 static ssize_t s_store(struct kobject*,
 			struct kobj_attribute*, const char*,size_t);
+
+static ssize_t c_show(struct kobject*,
+			struct kobj_attribute*,char*);
+static ssize_t c_store(struct kobject*,
+			struct kobj_attribute*, const char*,size_t);
+
+static ssize_t t_show(struct kobject*,
+			struct kobj_attribute*,char*);
+static ssize_t t_store(struct kobject*,
+			struct kobj_attribute*, const char*,size_t);
+
+
 
 static ssize_t s_show(struct kobject *kobject,
 			struct kobj_attribute *attr, char *buf)
@@ -45,14 +58,55 @@ static ssize_t s_store(struct kobject *kobject,
 	memcpy(test,buf,strlen(buf));
 	return count;
 }
-static struct kobj_attribute hw_attr =
+
+
+static ssize_t s_show(struct kobject *kobject,
+			struct kobj_attribute *attr, char *buf)
+{
+	
+	return sprintf(buf,"String");
+}
+static ssize_t s_store(struct kobject *kobject,
+			struct kobj_attribute *attr,const char *buf, size_t count)
+{
+	return count;
+}
+static ssize_t s_show(struct kobject *kobject,
+			struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf,"String");
+}
+static ssize_t s_store(struct kobject *kobject,
+			struct kobj_attribute *attr,const char *buf, size_t count)
+{
+	return count;
+}
+
+
+static struct kobj_attribute hw_attr1 =
 		 __ATTR(swap_string,0660,s_show,s_store);
+
+static struct kobj_attribute hw_attr2 =
+		 __ATTR(calc,0660,c_show,c_store);
+
+static struct kobj_attribute hw_attr3 =
+		 __ATTR(tree,0660,t_show,t_store);
+
+static struct attribute *attrs[] = {
+       &hw_attr1.attr,
+       &hw_attr2.attr,
+       &hw_attr3.attr,
+       NULL,   /* need to NULL terminate the list of attributes */
+};	 
+static struct attribute_group attr_group = {
+       .attrs = attrs,
+};
 static int __init hw_init(void)
 {
 	int retval;
 	hw_kobject = kobject_create_and_add("hw1",kernel_kobj);
 	
-	retval = sysfs_create_file(hw_kobject, &hw_attr.attr);
+	retval = sysfs_create_file(hw_kobject, &attr_group);
 	return retval;
 }
 static void __exit hw_exit(void)
